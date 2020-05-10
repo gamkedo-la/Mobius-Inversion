b57cb888426f92e1eceaa2f3b9cc8363adc44e54
Justin Chin <jchingames@gmail.com>
2020-05-10 11:05:52 -0700

    Player now respawns at start position on death

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,15 +9,26 @@ public class PlayerControl : MonoBehaviour
     private float horizSpeed = 14.0f;
     private float vertSpeed = 11.3f;
 
+    private bool isDead = false;
+    private Renderer myRend;
+
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRend = GetComponentInChildren<Renderer>();
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isDead)
+        {
+            return;
+        }
+
         transform.position += Vector3.up * Input.GetAxis("Vertical") * vertSpeed * Time.deltaTime +
                 Vector3.right * Input.GetAxis("Horizontal") * horizSpeed * Time.deltaTime;
 
@@ -27,10 +38,38 @@ public class PlayerControl : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D other)
     {
+        if(isDead)
+        {
+            return;
+        }
+
         if (other.gameObject.layer == LayerMask.NameToLayer("EnemyBullets"))
         {
-            Destroy(gameObject);
+            DeathThenRespawn();
         }
     }
 
+    void DeathThenRespawn()
+    {
+        if(isDead)
+        {
+            return;
+        }
+
+        isDead = true;
+        Debug.Log("Turn off stuff");
+        myRend.enabled = false;
+
+        StartCoroutine(Respawn());
+    }
+
+    IEnumerator Respawn ()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        isDead = false;
+        Debug.Log("Turn on stuff");
+        myRend.enabled = true;
+        transform.position = startPos;
+    }
 }
