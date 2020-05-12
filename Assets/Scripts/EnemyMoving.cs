94d31176920a350df993092f8b1f6be6733a856f
Justin Chin <jchingames@gmail.com>
2020-05-11 21:14:08 -0700

    Enemy now moves up and down while shooting!

---
+++ Assets/Scripts/EnemyMoving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoving : MonoBehaviour
+{
+
+    private Vector3 pos1 = new Vector3(18, -15, 20);
+    private Vector3 pos2 = new Vector3(18, 15, 20);
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
+
