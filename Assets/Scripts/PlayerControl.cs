649555f6e949e8a86e437c30dc59810a18ee8929
Andrew Mushel <almushel@gmail.com>
2020-05-13 11:38:12 -0500

    Clamp player ship position to screen boundaries.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public GameObject shotPrefab;
-	[SerializeField] Canvas pauseScreen;
+	[SerializeField] private Vector2 boundaryPadding = new Vector2(15.0f, 10.0f);
 
     private float horizSpeed = 14.0f;
     private float vertSpeed = 11.3f;
@@ -13,11 +13,13 @@ public class PlayerControl : MonoBehaviour
     private bool isDead = false;
     private Renderer myRend;
 
+	private Camera mainCamera;
     private Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
+		mainCamera = (Camera) GameObject.FindWithTag("MainCamera").GetComponent(typeof(Camera));
         myRend = GetComponentInChildren<Renderer>();
         startPos = transform.position;
         StartCoroutine(AutoShoot());
@@ -34,7 +36,9 @@ public class PlayerControl : MonoBehaviour
         transform.position += Vector3.up * Input.GetAxis("Vertical") * vertSpeed * Time.deltaTime +
                 Vector3.right * Input.GetAxis("Horizontal") * horizSpeed * Time.deltaTime;
 
-        /*if(Input.GetKeyDown(KeyCode.Space)) {        
+		transform.position = ClampToScreen(transform.position, mainCamera, boundaryPadding);
+		
+		/*if(Input.GetKeyDown(KeyCode.Space)) {        
             GameObject shotGO = GameObject.Instantiate(shotPrefab, transform.position, Quaternion.identity);
         }*/
 
@@ -42,14 +46,22 @@ public class PlayerControl : MonoBehaviour
 		{
 			TogglePause();
 		}
-        
     }
+
+	Vector3 ClampToScreen(Vector3 position, Camera camera, Vector2 padding) {
+		Vector3 screenPosition = camera.WorldToScreenPoint(position);
+		Vector3 clampedPosition = new Vector3(Mathf.Clamp(screenPosition.x, padding.x, camera.pixelWidth - padding.x), 
+											Mathf.Clamp(screenPosition.y, padding.x, camera.pixelHeight - padding.y), screenPosition.z);
+		
+		return camera.ScreenToWorldPoint(clampedPosition);
+	}
 
 	void TogglePause() 
 	{
 		if (Time.timeScale > 0) Time.timeScale = 0;
 		else Time.timeScale = 1.0f;
 	}
+	
     void OnCollisionEnter2D(Collision2D other)
     {
         if(isDead)
