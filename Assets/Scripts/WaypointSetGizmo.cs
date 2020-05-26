8bd91366e3dd054e863f7afd0fa8106084b79e3e
Andrew Mushel <almushel@gmail.com>
2020-05-26 11:41:05 -0500

    Selected WaypointSet paths now drawn in editor.

---
+++ Assets/Scripts/WaypointSetGizmo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WaypointSetGizmo : MonoBehaviour
+{
+	private Transform[] waypoints;
+
+	public void OnDrawGizmosSelected() {
+		waypoints = GetComponentsInChildren<Transform>();
+		Debug.Log(waypoints);
+		int length = waypoints.Length;
+		Gizmos.color = Color.red;
+		for (int i = 0; i < length; i++) {
+			//Gizmos.DrawSphere(waypoints[i].position, 1);
+			if (i < length - 1) Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
+			else break;
+		}
+	}
+}
