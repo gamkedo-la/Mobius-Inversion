ec8e5b627186fdfe97b888ba4bca45ae735842f4
Justin Chin <jchingames@gmail.com>
2020-05-30 20:47:58 -0700

    Bullets shooting forward!

--- Assets/Scripts/EnemyShot.cs
+++ Assets/Scripts/EnemyShot.cs
@@ -7,12 +7,14 @@ public class EnemyShot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 4.0f);   
+        Destroy(gameObject, 4.0f);
+        transform.Rotate(0f, -90f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         transform.position += Time.deltaTime * 20.0f * gameObject.transform.forward;
        
     }
