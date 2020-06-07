7b55522c43d8ba2cf3f5c8f87e94008a3d0512ec
Justin Chin <jchingames@gmail.com>
2020-06-07 10:00:21 -0700

    Waypoints smooth out differing speeds between each other

--- Assets/Scripts/EnemyMoving.cs
+++ Assets/Scripts/EnemyMoving.cs
@@ -14,14 +14,31 @@ public class EnemyMoving : MonoBehaviour
     private float progressPerk = 0.0f;
 
     public AnimationCurve speedCurve;
+    private WaypointData currentWayPointData = null;
+    private WaypointData nextWayPointData = null;
+
+    public float speedPerkHere;
 
     void Start()
     {
         waypointList = new List<Transform>();
-        Debug.Log(gameObject.name + " " + (waypointSet != null));
+        //Debug.Log(gameObject.name + " " + (waypointSet != null));
         for(int i = 0; i < waypointSet.childCount; i++)
         {
             waypointList.Add(waypointSet.GetChild(i));
+        }
+        UpdateCurrentIndexedWaypoint();
+    }
+
+    void UpdateCurrentIndexedWaypoint()
+    {
+        if (currentWayPoint < waypointList.Count)
+        {
+            currentWayPointData = waypointList[currentWayPoint].GetComponent<WaypointData>();
+            if (currentWayPoint < waypointList.Count - 1)
+            {
+                nextWayPointData = waypointList[currentWayPoint + 1].GetComponent<WaypointData>();
+            }
         }
     }
 
@@ -70,17 +87,22 @@ public class EnemyMoving : MonoBehaviour
         {
             return; //off the right side of the screen            
         }
-        progressPerk += Time.deltaTime * speed;
+        float smoothPerk = Ease(progressPerk);
+
+        speedPerkHere = Mathf.Lerp(currentWayPointData.speedPerc, nextWayPointData.speedPerc, progressPerk);
+        
+        progressPerk += Time.deltaTime * speed * speedPerkHere;
         if(progressPerk >= 1.0f)
         {
             progressPerk -= 1.0f;
             currentWayPoint++;
+            UpdateCurrentIndexedWaypoint();
         }
         if(currentWayPoint >= waypointList.Count - 1)
         {
             return;
         }
-        float smoothPerk = Ease(progressPerk);
+        
         Vector3 pos = Vector3.Lerp(waypointList[currentWayPoint].position, waypointList[currentWayPoint + 1].position, smoothPerk);     
         Quaternion rot = Quaternion.Slerp(waypointList[currentWayPoint].rotation, waypointList[currentWayPoint + 1].rotation, smoothPerk);
         transform.position = pos;
