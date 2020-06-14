78f55afb150a8b8f21eb924d0910c279ffe3f01f
Gamkedo <chris@gamkedo.com>
2020-06-14 12:39:52 -0700

    Adding in some voxel buildings from HP2's future scene

---
+++ Assets/Spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float spinRate = 30.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, spinRate * Time.deltaTime);
+    }
+}
