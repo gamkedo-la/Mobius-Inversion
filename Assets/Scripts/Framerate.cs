933ba482f8141e4daa6a176ab5c3057c939fd667
deepbluezen <deepbluezen@users.noreply.github.com>
2020-06-27 22:26:24 +0100

    Added framerate limiting, default 60fps, toggles between 30 and 60, hold r to show UI

---
+++ Assets/Scripts/Framerate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Framerate : MonoBehaviour
+{
+    Toggle Toggle60;
+    private  bool FramerateHigh;
+   
+    void Start()
+    {
+        
+        Toggle60 = GetComponent<Toggle>();
+        Toggle60.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(Toggle60);
+        });
+
+        //Set initial frame rate to 60, vsync turned off - Note I could not get Unity to respect the project level vsync settings for some reason
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+        FramerateHigh = true;             
+    }
+
+    public void ToggleValueChanged (bool toggle )
+    {
+        if (FramerateHigh == true)
+            {Application.targetFrameRate = 30;
+            }
+
+        else 
+            {
+            Application.targetFrameRate = 60;
+            }
+        
+        FramerateHigh = !FramerateHigh;
+    }
+
+
+    
+}
---
+++ Assets/Scripts/ShowMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMenu : MonoBehaviour
+{
+    
+    [SerializeField]
+    private CanvasGroup framerateCanvas;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            Show();
+        }
+
+        if (Input.GetKeyUp("r"))
+        {
+            Hide();
+        }
+
+    }
+
+
+    void Show() {
+    framerateCanvas.alpha = 1f;
+    framerateCanvas.blocksRaycasts = true;
+    framerateCanvas.interactable = true;
+    }
+
+    void Hide() {
+    framerateCanvas.alpha = 0f;
+    framerateCanvas.blocksRaycasts = false;
+    framerateCanvas.interactable = false;
+    }
+
+}
