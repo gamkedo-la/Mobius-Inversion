50ae998d6ba00d30d0f22a3d8f236b00169b4cff
deepbluezen <deepbluezen@users.noreply.github.com>
2020-07-01 22:42:16 +0100

    Explosion vfx update, 2ndry particles , shiny spinning parts

--- Assets/ExplosionHit.cs
+++ Assets/ExplosionHit.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class ExplosionHit : MonoBehaviour
 {
-   
-
+    [SerializeField]
+    private float force = 20.0f;
+     [SerializeField]
+    private float spin = 3.0f;
+    [SerializeField]
+    private Rigidbody2D enemyPart1;
+    [SerializeField]
+    private Rigidbody2D enemyPart2;
     void Awake()
     {
-        //Add in impulse to enemy craft parts
+        //assumes parts are going to only use 2D physics
+        enemyPart1.AddForce(Random.insideUnitCircle.normalized*force, ForceMode2D.Impulse);
+        enemyPart1.AddTorque(spin, ForceMode2D.Impulse);
+        enemyPart2.AddForce(Random.insideUnitCircle.normalized*force, ForceMode2D.Impulse);
+        enemyPart2.AddTorque(-spin, ForceMode2D.Impulse);
     }
     
     void OnParticleSystemStopped()
