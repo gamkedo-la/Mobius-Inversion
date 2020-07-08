4c3b1421911a9f89a97b994137f6750459329f41
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-07-08 15:38:57 -0700

    Script to spawn enemies in a grid
    Currently the script can be placed on an empty game object to spawn a set of enemies in a grid at the scene start and parent them to that game object. The script also passes a waypoint set to the newly spawned enemies.
    
    Currently you can edit the number of columns and rows as well as the space between them, and what waypoint set will be given to the spawned enemies.
    
    I am working on a way to show where the enemies will be spawned in the editor window but it is proving to be difficult.

---
+++ Assets/Scripts/EnemyGridSpawnerTool.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnemyGridSpawnerTool : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public int columns;
+    public float HorizontalSpacing;
+    public int rows;
+    public float VerticalSpacing;
+
+    public Transform Waypoints;
+
+    Vector3 temp = new Vector3(0, 0, 0);
+
+
+    [ContextMenu("Preview Spawns")]
+    void preview()
+    {
+
+        spawnGird();
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+          /*  foreach (Transform child in gameObject.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        */
+
+        spawnGird();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnGird()
+    {
+
+        temp = gameObject.transform.position;
+
+        for (int c = 1; c <= columns; c++)
+        {
+
+            for (int r = 0; r < rows; r++)
+            {
+
+                GameObject E = (GameObject)Instantiate(
+                           enemy,
+                           temp,
+                           Quaternion.identity,
+                           gameObject.transform);
+
+                if (E.GetComponent<EnemyMoving>() != null)
+                {
+                    E.GetComponent<EnemyMoving>().waypointSet = Waypoints;
+                }
+                temp.y -= VerticalSpacing;
+
+            }
+            temp = gameObject.transform.position;
+            temp.x += (c * HorizontalSpacing);
+        }
+    }
+
+
+}
