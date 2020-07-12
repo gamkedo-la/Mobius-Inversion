dad7609d9283d107bdf35c633024dd82337eab30
Justin Chin <jchingames@gmail.com>
2020-07-12 13:05:24 -0700

    Got old commit with all changes up to reverts to reset the week.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-
+    public GameObject enemyExplosion;
     public int maxHealth = 100;
     public int currentHealth;
     private PlayerControl pcScript;
 
     public HealthBarScript healthBar;
+    public float shieldTimeUp = 1.0f;
+    private float shieldLastTime = 4.0f;
+    private PlayerShield shield;
 
     // Start is called before the first frame update
     void Start()
     {
         pcScript = GetComponent<PlayerControl>();
+        shield = GetComponentInChildren<PlayerShield>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
@@ -23,21 +27,38 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+      if(shield && shieldTimeUp > 0)
+        {
+            shieldTimeUp -= Time.deltaTime / shieldLastTime;
+            if(shieldTimeUp <= 0)
+            {
+                shield.gameObject.SetActive(false);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         //Debug.Log("Player touched by " + other.gameObject.name);
 
+        if (shieldTimeUp > 0)
+        {
+            return;
+        }
+
         if (other.gameObject.layer == LayerMask.NameToLayer("EnemyBullets"))
         {
+            Instantiate(enemyExplosion, transform.position, transform.rotation);
             TakeDamage(20);
+            Destroy(other.gameObject);
         }
         if (other.gameObject.layer == LayerMask.NameToLayer("EnemyShip"))
         {
+            Instantiate(enemyExplosion, transform.position, transform.rotation);
             TakeDamage(20);
+            Destroy(other.gameObject);
         }
+
     }
 
     void TakeDamage(int damage)
