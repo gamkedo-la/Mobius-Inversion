6944f50f333266b772db4fcce92fa402a79b376b
deepbluezen <deepbluezen@users.noreply.github.com>
2020-07-30 22:31:45 +0100

    Explosion sound added and linked to wasp enemy death

--- Assets/Scripts/PlayerDestroy.cs
+++ Assets/Scripts/PlayerDestroy.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerDestroy : MonoBehaviour
 {
     public GameObject enemyExplosion;
+    public AudioSource shotAudio;
+    public AudioClip   shotClip;
+    public float shotVolume=0.5f;
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("PlayerBullets"))
         {
                         
             Instantiate(enemyExplosion, transform.position,transform.rotation);
-            Destroy(gameObject);
+            shotAudio.PlayOneShot(shotClip, shotVolume*Random.Range(0.8f,1.2f));
+            Destroy(gameObject,1);
             Destroy(other.gameObject);
             
         }
