2fead3197700ebdc3cc2049a7f26d20917c49cf3
deepbluezen <deepbluezen@users.noreply.github.com>
2020-08-05 08:43:03 +0100

    Audio mixer set up for background music and sounds

--- Assets/Scripts/PlayerDestroy.cs
+++ Assets/Scripts/PlayerDestroy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerDestroy : MonoBehaviour
 {
@@ -12,10 +13,11 @@ public class PlayerDestroy : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("PlayerBullets"))
         {
-                        
+           // Vector3 audiolocation = transform.position;           
             Instantiate(enemyExplosion, transform.position,transform.rotation);
+            //shotAudio.PlayClipAtPoint(shotClip, audiolocation ,shotVolume*Random.Range(0.8f,1.2f));
             shotAudio.PlayOneShot(shotClip, shotVolume*Random.Range(0.8f,1.2f));
-            Destroy(gameObject,1);
+            Destroy(gameObject, 1);
             Destroy(other.gameObject);
             
         }
