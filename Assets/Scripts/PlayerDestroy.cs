6226de5ae55c2696a63708585e656df5ab6bbef6
deepbluezen <deepbluezen@users.noreply.github.com>
2020-08-08 22:27:03 +0100

    Background splash screen for use behind UI on load of game - to be integrated

--- Assets/Scripts/PlayerDestroy.cs
+++ Assets/Scripts/PlayerDestroy.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
+
 
 public class PlayerDestroy : MonoBehaviour
 {
@@ -13,11 +13,15 @@ public class PlayerDestroy : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("PlayerBullets"))
         {
-           // Vector3 audiolocation = transform.position;           
+           Vector3 audiolocation = transform.position;
+           audiolocation.z = -10;           
             Instantiate(enemyExplosion, transform.position,transform.rotation);
-            //shotAudio.PlayClipAtPoint(shotClip, audiolocation ,shotVolume*Random.Range(0.8f,1.2f));
+            //AudioSource.PlayClipAtPoint(shotClip, audiolocation ,shotVolume*Random.Range(0.8f,1.2f));
+           //Destroy(gameObject);
+           //Think above plays but very quietly, need to check where in 3d space this is actually playing is it too far from camera to here?
+           //Try writing in camera vector 3 to see if plays at expected volume
             shotAudio.PlayOneShot(shotClip, shotVolume*Random.Range(0.8f,1.2f));
-            Destroy(gameObject, 1);
+            Destroy(gameObject,1);
             Destroy(other.gameObject);
             
         }
