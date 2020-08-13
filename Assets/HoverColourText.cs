58619de5b959fe5f46ced025c134a065edd46623
deepbluezen <deepbluezen@users.noreply.github.com>
2020-08-13 22:01:57 +0100

    Change colour of UI text on button hover

---
+++ Assets/HoverColourText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+
+public class HoverColourText : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject textcolour;
+    public void onHover()
+    {
+        TextMeshProUGUI textmeshPro = textcolour.GetComponent<TextMeshProUGUI>();
+        textmeshPro.color = new Color32(255, 214, 53, 255);
+       //Debug.Log("hover");
+    }
+
+    public void onExit()
+    {
+        TextMeshProUGUI textmeshPro = textcolour.GetComponent<TextMeshProUGUI>();
+        textmeshPro.color = new Color32(255, 91, 24, 255);
+       //Debug.Log("hover");
+    }
+}
