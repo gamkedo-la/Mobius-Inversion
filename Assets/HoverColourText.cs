62324857a91c1c533554dc76ce3fae8eb272ceb2
deepbluezen <deepbluezen@users.noreply.github.com>
2020-08-15 17:52:11 +0100

    Hooked up other start screen buttons - scene change added to code - placeholder scenes to link too added

--- Assets/HoverColourText.cs
+++ Assets/HoverColourText.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 using System;
 
@@ -13,13 +14,29 @@ public class HoverColourText : MonoBehaviour
     {
         TextMeshProUGUI textmeshPro = textcolour.GetComponent<TextMeshProUGUI>();
         textmeshPro.color = new Color32(255, 214, 53, 255);
-       //Debug.Log("hover");
+       
     }
 
     public void onExit()
     {
         TextMeshProUGUI textmeshPro = textcolour.GetComponent<TextMeshProUGUI>();
         textmeshPro.color = new Color32(255, 91, 24, 255);
-       //Debug.Log("hover");
+       
     }
+
+    public void gotoMainScene()
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    
+    public void gotoTutorialScene()
+        {
+            SceneManager.LoadScene("TutorialScene_Andy");
+        }
+    
+    public void gotoOptionsScene()
+        {
+            SceneManager.LoadScene("OptionsScene_Andy");
+        }
+
 }
