5252d95e7bc2e9310cff7327b25e2973976fe524
deepbluezen <deepbluezen@users.noreply.github.com>
2020-08-16 15:28:20 +0100

    Marker for selection and UI sound on button click added

--- Assets/HoverColourText.cs
+++ Assets/HoverColourText.cs
@@ -12,33 +12,42 @@ public class HoverColourText : MonoBehaviour
     [SerializeField] private GameObject textcolour;
     [SerializeField] private AudioSource UIaudiosource;
     [SerializeField] private AudioClip   UIbeep;
+    [SerializeField] private AudioClip   UIclick;
+    [SerializeField] private GameObject  marker;
     public void onHover()
     {
         TextMeshProUGUI textmeshPro = textcolour.GetComponent<TextMeshProUGUI>();
         textmeshPro.color = new Color32(255, 214, 53, 255);
         UIaudiosource.PlayOneShot(UIbeep, 0.5f);
+        marker.SetActive(true);
     }
 
     public void onExit()
     {
         TextMeshProUGUI textmeshPro = textcolour.GetComponent<TextMeshProUGUI>();
         textmeshPro.color = new Color32(255, 91, 24, 255);
-       
+         marker.SetActive(false);
     }
+
+  
 
     public void gotoMainScene()
         {
-            SceneManager.LoadScene("MainScene");
+            UIaudiosource.PlayOneShot(UIclick, 0.5f);
+            SceneManager.LoadSceneAsync("MainScene");
         }
     
     public void gotoTutorialScene()
         {
-            SceneManager.LoadScene("TutorialScene_Andy");
+            UIaudiosource.PlayOneShot(UIclick, 0.5f);
+            SceneManager.LoadSceneAsync("TutorialScene_Andy");
         }
     
     public void gotoOptionsScene()
         {
-            SceneManager.LoadScene("OptionsScene_Andy");
+            UIaudiosource.PlayOneShot(UIclick, 0.5f);
+            SceneManager.LoadSceneAsync("OptionsScene_Andy");
         }
+    
 
 }
