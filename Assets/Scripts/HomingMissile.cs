ffd412934c2fd406d3de070e568d5edd9a7bef9c
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-08-18 16:43:31 -0700

    Fixed homing missles, put the boss models into the scene

--- Assets/Scripts/HomingMissile.cs
+++ Assets/Scripts/HomingMissile.cs
@@ -28,7 +28,9 @@ public class HomingMissile : MonoBehaviour
 
         gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Target.transform.position, step);
 
-        transform.LookAt(Target.transform);
+        Vector3 dir = Target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle +180 , Vector3.forward);
 
     }
 
