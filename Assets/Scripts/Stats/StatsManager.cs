7be7879ee6c7bc75e57793f8cb1e20480687a1a0
Philip Greene <philip_greene@hotmail.com>
2020-08-31 01:11:27 +0100

    Started on end stats scene - Added script to contain player stats

---
+++ Assets/Scripts/Stats/StatsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour
+{
+    public int enemyKills = 0;
+    public int playerDeaths = 0;
+    public int damageDealt = 0;
+    public int damageTaken = 0;
+
+    private void Start()
+    {
+        DisplayStats();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void DisplayStats()
+    {
+        
+    }
+    public void IncrementStat(int statistic, int value)
+    {
+        statistic += value;
+    }
+}
---
+++ Assets/Scripts/Stats/StatsSlider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsSlider : MonoBehaviour
+{
+    private float targetValue = 0.75f;
+    private float sliderStopSpeed = 0.0025f;
+    private bool sliderActive = true;
+    private Slider elimSlider;
+    private float fillSpeed = 0.0f;
+
+    private void Start()
+    {
+        elimSlider = GetComponent<Slider>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (sliderActive)
+        {
+            UpdateSlider();
+        }
+    }
+
+    private void StartSlider()
+    {
+        sliderActive = true;
+    }
+
+    private void UpdateSlider()
+    {
+        if (elimSlider.value < targetValue)
+        {
+            fillSpeed = targetValue - elimSlider.value; // Decelerate slider
+            elimSlider.value += fillSpeed * Time.deltaTime;
+
+            // Stop slider once it has decelerated to stopping speed
+            if (fillSpeed <= sliderStopSpeed)
+            {
+                StopSlider();
+            }
+        }
+    }
+    private void StopSlider()
+    {
+        fillSpeed = 0.0f;
+        elimSlider.value = targetValue;
+        sliderActive = false;
+    }
+}
