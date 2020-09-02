d230f7316f36c79f9b670cbce494cf34f17efa28
deepbluezen <deepbluezen@users.noreply.github.com>
2020-09-02 21:00:51 +0100

    Capture of training examples to build from

---
+++ Assets/PanelAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelAnimator : MonoBehaviour
+{
+    public RectTransform panel;
+    public GameObject panelBG;
+
+    public void ShowLevelSelectPanel()
+    {
+        StartCoroutine(PanelAnimation());
+    }
+
+    private IEnumerator PanelAnimation()
+    {
+        panelBG.SetActive(true);
+        float timer = 0f;
+        while (timer < 1f)
+        {
+            timer += Time.deltaTime;
+            //this code will run for 1s exact
+            panel.localScale = Vector3.one * Mathf.LerpUnclamped(0f, 1f, Easings.ElasticEaseOut(timer));
+            yield return null;
+        }
+    }
+
+}
