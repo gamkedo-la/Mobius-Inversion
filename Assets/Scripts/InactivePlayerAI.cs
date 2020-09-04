b1f95d1603e58a3d3dd94d63d44e8b09017bf1d5
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-09-04 16:04:15 -0700

    Inactive player AI for the boss

--- Assets/Scripts/InactivePlayerAI.cs
+++ Assets/Scripts/InactivePlayerAI.cs
@@ -9,7 +9,9 @@ public class InactivePlayerAI : MonoBehaviour
 
     public List<GameObject> EnemyList = new List<GameObject>();
 
+    public List<GameObject> ShieldList = new List<GameObject>();
 
+    GameObject Boss;
 
     GameObject Target;
 
@@ -30,36 +32,62 @@ public class InactivePlayerAI : MonoBehaviour
     void Update()
     {
         EnemyList.Clear();
+        ShieldList.Clear();
+
+        Boss = GameObject.Find("Boss");
 
 
         if(gameObject.GetComponent<PlayerControl>().activeShip != thisShipColor)
         {
-
-
-            if (Target == null || Target.transform.position.x < gameObject.transform.position.x)
+            if (Boss != null)
             {
-
-
-
-                foreach (GameObject enObj in GameObject.FindGameObjectsWithTag("Enemy"))
+                foreach (GameObject enObj in GameObject.FindGameObjectsWithTag("BossShield"))
                 {
-
-                    EnemyList.Add(enObj);
+                    ShieldList.Add(enObj);
                 }
 
-
-                EnemyList.Sort(delegate (GameObject A, GameObject B)
+                for( int i = 0; i < ShieldList.Count; i++)
                 {
-                    return Vector2.Distance(this.transform.position, A.transform.position)
-                      .CompareTo
-                      (Vector2.Distance(this.transform.position, B.transform.position));
-                });
 
-                Target = EnemyList[Random.Range(0, 2)];
+                    if(ShieldList[i].GetComponent<BossShield>().PassThrough == gameObject.GetComponent<PlayerControl>().thisShip)
+                    {
+                        Target = ShieldList[i];
 
+                    }
+
+                }
+            }
+            else
+            {
+                if (Target == null || Target.transform.position.x < gameObject.transform.position.x)
+                {
+                    foreach (GameObject enObj in GameObject.FindGameObjectsWithTag("Enemy"))
+                    {
+
+                        EnemyList.Add(enObj);
+                    }
+
+                    EnemyList.Sort(delegate (GameObject A, GameObject B)
+                    {
+                        return Vector2.Distance(this.transform.position, A.transform.position)
+                          .CompareTo
+                          (Vector2.Distance(this.transform.position, B.transform.position));
+                    });
+
+                    Target = EnemyList[Random.Range(0, 2)];
+
+                }
             }
 
-            Vector3 dest = new Vector3(gameObject.transform.position.x, Target.transform.position.y, 20);
+            if (Target != null)
+            {
+                Vector3 dest = new Vector3(gameObject.transform.position.x, Target.transform.position.y, 20);
+            }
+            else
+            {
+                Vector3 dest = new Vector3(gameObject.transform.position.x, (gameObject.transform.position.y + Random.Range(-3,3)), 20);
+
+            }
 
             float step = gameObject.GetComponent<PlayerControl>().vertSpeed * Time.deltaTime * 0.7f;
 
