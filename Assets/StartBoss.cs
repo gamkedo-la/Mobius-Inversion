a5466b48e040ddb409363aae64577fc38dfff30f
Justin Chin <jchingames@gmail.com>
2020-09-10 20:39:30 -0700

    Boss in the scene and triggers after last enemy

---
+++ Assets/StartBoss.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBoss : MonoBehaviour
+{
+    public StartBossFightAfterLevelEnd sBALE;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < Bounds.instance.rightX)
+        {
+            sBALE.WakeBoss();
+            this.enabled = false;
+        }
+    }
+}
---
+++ Assets/StartBossFightAfterLevelEnd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBossFightAfterLevelEnd : MonoBehaviour
+{
+    public GameObject bossToWake;
+    public ScrollWorld scrollToStop;
+
+    // Update is called once per frame
+    public void WakeBoss()
+    {
+        if(bossToWake != null && bossToWake.activeSelf == false)
+        {
+            bossToWake.SetActive(true);
+            scrollToStop.enabled = false;
+
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
