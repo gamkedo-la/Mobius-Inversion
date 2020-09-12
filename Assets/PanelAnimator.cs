00e327371e0e51dfa8a0b767f93bbb231c9c5796
deepbluezen <deepbluezen@users.noreply.github.com>
2020-09-12 16:59:27 +0100

    Tutorial scene Names added to text, animation script added

--- Assets/PanelAnimator.cs
+++ Assets/PanelAnimator.cs
@@ -3,27 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PanelAnimator : MonoBehaviour
 {
-    public RectTransform panel;
-    public GameObject panelBG;
+    public RectTransform panelrect;
+    public RectTransform panel2rect;
+    public GameObject panel2;
+    
 
-    public void ShowLevelSelectPanel()
+    /* public void ShowLevelSelectPanel()
     {
         // StartCoroutine(PanelAnimation());
         SceneManager.LoadScene("MainScene");
+    } */
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            StartCoroutine(PanelAnimationOutBack());
+            StartCoroutine(PanelAnimationInFront());
+             }
+
     }
 
-    private IEnumerator PanelAnimation()
+    private IEnumerator PanelAnimationOutBack()
     {
-        panelBG.SetActive(true);
+        //panelBG.SetActive(true);
         float timer = 0f;
         while (timer < 1f)
         {
             timer += Time.deltaTime;
             //this code will run for 1s exact
-            panel.localScale = Vector3.one * Mathf.LerpUnclamped(0f, 1f, Easings.ElasticEaseOut(timer));
+            panelrect.localScale = Vector3.one * Mathf.LerpUnclamped(0.5f, 0f, Easings.QuadraticEaseOut(timer));
             yield return null;
+        }
+    }
+
+    private IEnumerator PanelAnimationInFront()
+    {
+         yield return new WaitForSeconds(0.5f);
+        panel2.SetActive(true);
+        float timer = 0f;
+        while (timer < 1f)
+        {
+            timer += Time.deltaTime;
+            //this code will run for 1s exact
+            panel2rect.localScale = Vector3.one * Mathf.LerpUnclamped(2.0f, 0.5f, Easings.QuadraticEaseIn(timer));
+               yield return null;
         }
     }
 
